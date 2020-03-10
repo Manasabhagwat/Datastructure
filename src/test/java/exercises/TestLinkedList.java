be63a04c404b3fc9ecdd708ff3340ebeed2e92ad
Philipp Haller <hallerp@gmail.com>
2020-03-10 10:15:23 +0100

    Add placeholder for unit tests for LinkedList
    This placeholder is going to contain all unit tests for
    methods of LinkedList.

---
+++ src/test/java/exercises/TestLinkedList.java
@@ -0,0 +1,11 @@
+package exercises;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class TestLinkedList {
+    @Test
+    public void test1() {
+    }
+}
